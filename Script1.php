4616c9a928131b14cf77b453e5cc9ce5a2de9969
BatrinutSilviu <batranutsilviu@yahoo.com>
2018-12-04 19:56:29 +0200

    versiunea2

--- Script1.php
+++ Script1.php
@@ -1,8 +1,10 @@
 <?php
 $conn =  mysql_connect("localhost","root","") or die("Unable to connect to MySQL");
 mysql_select_db("magazin") or die("nu se gasesti bd");
+echo "conectat la server si baza de date";
 $temp=$_POST['temperature'];
 $umid=$_POST['humidity'];
-mysql_query("insert into DATC values($temp,$umid)");
-
+echo "$temp si $umid sunt de inserat";
+mysql_query("insert into DATC values($temp,$umid)") or die("INSERT Query has Failed");
+echo "adaugare finalizata";
 ?>
