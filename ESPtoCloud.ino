6610b5281e43884508362a11fa21aabef0deaf87
BatrinutSilviu <batranutsilviu@yahoo.com>
2018-12-08 21:44:53 +0200

    CodTransmisieWiFi

--- ESPtoCloud.ino
+++ ESPtoCloud.ino
@@ -1,13 +1,16 @@
+#include "DHT.h"
 #include "WiFi.h"
 #include "HTTPClient.h"
+
 #define RETRY_LIMIT  20
 const char* ssid = "Silviu";
 const char* password = "casino17";
+DHT dht1(2, DHT11);
 
 void setup()
 {
- 
     Serial.begin(9600);
+    dht1.begin();
     WiFi.begin(ssid,password);
     while (WiFi.status()!= WL_CONNECTED){
       delay(500);
@@ -23,18 +26,23 @@ void setup()
 void loop()
 {
     int rtl = RETRY_LIMIT;
-    int t=2;
-    int h=3;
-    //Open a connection to the server
+    delay(3000);
+ 
+    float t = dht1.readTemperature();
+    float h = dht1.readHumidity();
+    Serial.print("DHT11  ");
+    Serial.print(t,1); Serial.print(String(char(176))+"C  ");
+    Serial.print(h,1); Serial.println("%RH");
+    Serial.println();
+    
     HTTPClient http;
-    http.begin("http://192.168.1.101/EXPAI/Script2.php");
+    http.begin("http://192.168.1.102/EXPAI/Script2.php");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    //format your POST request.
-      int httpResponseCode = http.POST("temperature=" + String(t) +"& humidity=" + String(h));
+    int httpResponseCode = http.POST("temperature=" + String(t) +"& humidity=" + String(h));
 
       if (httpResponseCode >0){
-          //check for a return code - This is more for debugging.
         String response = http.getString();
+        Serial.print("Sent with code ");
         Serial.println(httpResponseCode);
         Serial.println(response);
       }
@@ -42,16 +50,12 @@ void loop()
         Serial.print("Error on sending post");
         Serial.println(httpResponseCode);
       }
-    //closde the HTTP request.
       http.end();
   
-    //Monitor values in console for debugging.
       Serial.println("Temp = " + String(t));
       Serial.println("humidity = " + String(h));
     
-    
-   //wait 2 minutes for next reading
-    delay(120000);
-    
+    //delay(120000);
+    delay(2000);
     
 }
